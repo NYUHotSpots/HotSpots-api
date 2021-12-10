9f82e8479cce87df11ee35b51b5813a46c70c0f9
Kevin Chen <kc3585@nyu.edu>
2021-12-10 15:54:55 -0500

    add github actions

--- db/tests/test_data.py
+++ db/tests/test_data.py
@@ -3,12 +3,7 @@ This file holds the tests for db.py.
 """
 
 from unittest import TestCase, skip
-# import random
-
 import db.data as db
-
-FAKE_USER = "Fake user"
-
 
 class DBTestCase(TestCase):
     def setUp(self):
@@ -16,13 +11,6 @@ class DBTestCase(TestCase):
 
     def tearDown(self):
         pass
-
-    def test_write_collection(self):
-        """
-        Can we write the user db?
-        """
-        fake_data = {FAKE_USER: {}}
-        return True
 
     def test_get_users(self):
         """
