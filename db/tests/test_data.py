843e184a007bd6ee7e7fbfc0609231d09358d017
Kevin Chen <kc3585@nyu.edu>
2022-02-11 08:15:08 +0000

    wrote tests for database functions

--- db/tests/test_data.py
+++ db/tests/test_data.py
@@ -11,3 +11,17 @@ class DBTestCase(TestCase):
 
     def tearDown(self):
         pass
+
+    def test_get_flavors(self):
+        """
+        Tests the database to see if we can get spots
+        """
+        spots = db.get_spots()
+        self.assertIsInstance(spots, dict)
+
+    def test_get_review(self):
+        """
+        Tests if we can get reviews of a spot
+        """
+        reviews = db.get_review_by_spot("61fb2daddeb53e7d70091516")
+        self.assertIsInstance(reviews, list)
