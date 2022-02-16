45d2089dcc26ed4b03f60565e8869dde33871b66
tiffanychan0909 <55333047+tiffanychan0909@users.noreply.github.com>
2022-02-16 12:59:29 -0500

    clean up util code

--- API/security/utils.py
+++ API/security/utils.py
@@ -21,17 +21,16 @@ def json_abort(status_code, message=None):
 
 def get_auth0_token():
     conn = http.client.HTTPSConnection("hotspots-dev.us.auth0.com")
-    payload = {
+    payload = json.dumps({
         "client_id" : client_id, 
         "client_secret" : client_secret, 
         "audience": audience,
         "grant_type": "client_credentials"
-    }
-    payload = json.dumps(payload)
+    })
     headers = { 'content-type': "application/json" }
     conn.request("POST", "/oauth/token", payload, headers)
 
     res = conn.getresponse()
-    data = json.loads(res.read())
-    print("New access token: " + data["access_token"])
-    return(data["access_token"])
+    access_token = json.loads(res.read())["access_token"]
+    print("New access token: " + access_token)
+    return access_token
