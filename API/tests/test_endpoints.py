b7f68b8666d57a43d751b7a68037ca57ae0e20f8
tiffanychan0909 <55333047+tiffanychan0909@users.noreply.github.com>
2022-02-25 10:51:20 -0500

    fix tests

--- API/tests/test_endpoints.py
+++ API/tests/test_endpoints.py
@@ -39,12 +39,12 @@ class EndpointTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         
     def test_get_all_spots(self):
-        response = self.client.get("/spot", headers=self.headers)
+        response = self.client.get("/spot/list", headers=self.headers)
         print(response.data)
         self.assertEqual(response.status_code, 200)
         
     def test_create_update_delete_spot(self):
-        response = self.client.post("/spot", data=self.spotData, headers=self.headers)
+        response = self.client.post("/spot/create", data=self.spotData, headers=self.headers)
         spot_id = response.data.decode("utf-8").strip().strip("\"")
         print("Test Create Spot", spot_id)
         self.assertEqual(response.status_code, 200)
@@ -53,42 +53,42 @@ class EndpointTestCase(TestCase):
         print("Test Get Spot", response.data)
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.put(f"/spot/{spot_id}", data=self.updatedSpotData, headers=self.headers)
+        response = self.client.put(f"/spot/update/{spot_id}", data=self.updatedSpotData, headers=self.headers)
         print("Test Update Spot", response.data)
         self.assertEqual(response.status_code, 200)
         
-        response = self.client.delete(f"/spot/{spot_id}", headers=self.headers)
+        response = self.client.delete(f"/spot/delete/{spot_id}", headers=self.headers)
         print("Test Delete Spot", response.data)
         self.assertEqual(response.status_code, 200)
         
     def test_create_review(self):
-        response = self.client.post("/spot", data=self.spotData, headers=self.headers)
+        response = self.client.post("/spot/create", data=self.spotData, headers=self.headers)
         print(response.data)
         spot_id = response.data.decode("utf-8").strip().strip("\"")
         print("Test Create Review (Make Spot First)", spot_id)
         self.assertEqual(response.status_code, 200)
         
         self.reviewData["spotID"] = spot_id
-        response = self.client.post("/review", data=self.reviewData, headers=self.headers)
+        response = self.client.post("/review/create", data=self.reviewData, headers=self.headers)
         review_id = response.data.decode("utf-8").strip().strip("\"")
         print("Test Create Review", review_id)
         print(response.data)
         self.assertEqual(response.status_code, 200)
         
-        response = self.client.get(f"/review/{spot_id}", headers=self.headers)
+        response = self.client.get(f"/review/read/{spot_id}", headers=self.headers)
         print("Test Get Review", response.data)
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.delete(f"/review/{review_id}", headers=self.headers)
+        response = self.client.delete(f"/review/delete/{review_id}", headers=self.headers)
         print("Test Delete Review", response.data)
         self.assertEqual(response.status_code, 200)
 
     def test_unauthorized_bad_requests(self):
-        response = self.client.post("/spot", data=self.spotData)
+        response = self.client.post("/spot/create", data=self.spotData)
         self.assertEqual(response.status_code, 401)
         
         bad_header = {"authorization" : "Bearer "}
-        response = self.client.post("/spot", data=self.spotData, headers=bad_header)
+        response = self.client.post("/spot/create", data=self.spotData, headers=bad_header)
         self.assertEqual(response.status_code, 400)
 
     def test_bad_deletes(self):
@@ -98,14 +98,14 @@ class EndpointTestCase(TestCase):
         (if you call delete and that doc isn't found, 
         then mongo will just pretend like it did delete it without raising any error)
         '''
-        response = self.client.delete(f"/review/1234", headers=self.headers)
+        response = self.client.delete(f"/review/delete/1234", headers=self.headers)
         self.assertEqual(response.status_code, 404)
         
-        response = self.client.delete(f"/review/{self.bad_id}", headers=self.headers)
+        response = self.client.delete(f"/review/delete/{self.bad_id}", headers=self.headers)
         self.assertEqual(response.status_code, 404)
         
-        response = self.client.delete(f"/spot/1234", headers=self.headers)
+        response = self.client.delete(f"/spot/delete/1234", headers=self.headers)
         self.assertEqual(response.status_code, 404)
         
-        response = self.client.delete(f"/spot/{self.bad_id}", headers=self.headers)
+        response = self.client.delete(f"/spot/delete/{self.bad_id}", headers=self.headers)
         self.assertEqual(response.status_code, 404)
